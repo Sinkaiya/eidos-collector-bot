7271f95bfb738ace3a79a49a3129da27b176af58
Sergey Novikov <sinkaiya@gmail.com>
2023-04-07 14:16:38 +0300

    get_from_db function update. new_chapter_id processing is ready.

--- main.py
+++ main.py
@@ -19,12 +19,14 @@ def add_user(username):
         cursor.execute(search_query, [username])
         # Adding a new user into the db:
         if cursor.fetchone() is None:
-            insert_query = "INSERT INTO `vda_users` (`telegram_id`,`last_chapter_sent`, `on_hold`) VALUES (%s, '0', '0');"
+            insert_query = "INSERT INTO `vda_users` (`telegram_id`,`last_chapter_sent`, `on_hold`) " \
+                           "VALUES (%s, '0', '0');"
             cursor.execute(insert_query, [username])
             connection.commit()
 
 
 def get_chapter(chapter_id):
+    # TODO реализовать это тоже через get_from_db
     select_query = "SELECT `chapter` FROM `vda_messages` WHERE `id`=(%s);"
     with connection.cursor() as cursor:
         cursor.execute(select_query, [chapter_id])
@@ -34,47 +36,50 @@ def get_chapter(chapter_id):
         return text
 
 
-def check_on_hold(user_id):
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT `on_hold` FROM `vda_users` WHERE `id` = %s;" % user_id)
-        result = cursor.fetchall()
-        on_hold_status = result[0][0]
-    if on_hold_status == 1:
-        return True
-    else:
-        return False
-
-
-# def db_update(table_name, cell_name, value, row_id):
-#     # Example: UPDATE `vda_users` SET `last_chapter_sent` = '2' WHERE `id`='2';
-#     # update_query = "UPDATE `" + \
-#     #                table_name + "` SET `" + \
-#     #                cell_name + "` = '" + \
-#     #                value + "' WHERE `id` = '" + \
-#     #                row_id + "';"
+# def check_on_hold(user_id):
+#     select_query = f"SELECT `on_hold` FROM `vda_users` WHERE `id` = %s;"
 #     with connection.cursor() as cursor:
-#         cursor.execute("UPDATE %s SET %s = %s WHERE `id` = %s;" % (table_name, cell_name, value, row_id))
-#         # cursor.execute(update_query)
-#         connection.commit()
+#         # cursor.execute("SELECT `on_hold` FROM `vda_users` WHERE `id` = %s;" % user_id)
+#         cursor.execute(select_query, [user_id])
+#         result = cursor.fetchall()
+#         on_hold_status = result[0][0]
+#     if on_hold_status == 1:
+#         return True
+#     else:
+#         return False
 
+
+def get_from_db(cell_name, table_name, row_id):
+    select_query = f"SELECT `{cell_name}` FROM `{table_name}` WHERE `id` = %s;"
+    with connection.cursor() as cursor:
+        cursor.execute(select_query, [row_id])
+        result = cursor.fetchall()
+        if cell_name == 'on_hold':
+            on_hold_status = result[0][0]
+            if on_hold_status == 1:
+                return True
+            else:
+                return False
+        elif cell_name == 'last_chapter_sent':
+            last_chapter_sent = result[0][0]
+            return last_chapter_sent
+        elif cell_name == 'chapter':
+            for row in result:
+                text = "".join(row[0].decode("utf8"))
+            return text
 
 def db_update(table_name, cell_name, value, row_id):
     with connection.cursor() as cursor:
-        query = "UPDATE {table_name} SET {cell_name} = %s WHERE `id` = %s;"
-        cursor.execute(query, (table_name, cell_name), value, row_id)
+        # TODO Может быть, всё запихнуть сразу в запрос через format? И в execute передавать только его?
+        update_query = f"UPDATE `{table_name}` SET {cell_name} = %s WHERE `id` = %s;"
+        cursor.execute(update_query, (value, row_id))
         connection.commit()
 
 
-
-# def db_update(table_name, column_name, value, row_id):
-#     with connection.cursor() as cursor:
-#         cursor.execute("UPDATE {table_name} SET {column_name} = %s WHERE id = %s;" .format(table_name, column_name, value), row_id)
-#         connection.commit()
-
-
 def db_table_rows_count(table_name):
+    select_query = f"SELECT COUNT(*) FROM `{table_name}`;"
     with connection.cursor() as cursor:
-        cursor.execute("SELECT COUNT(*) FROM %s;" % (table_name.upper()))
+        cursor.execute(select_query)
         result = cursor.fetchall()
         row_count = result[0][0]
         return row_count
@@ -95,22 +100,31 @@ try:
         #  кусочек текста. находящая в таблице с пользователями конкретного, проверяющая его
         #  on_hold-статус, находящая нужный текст в БД, отправляющая пользователю и обновляющая
         #  позицию на следующую.
+
         # [ ] 1. Проходим по таблице с пользователями. Вытаскиваем каждого пользователя по id.
         # Получаем количество пользователей.
-        # table_for_count = 'vda_users'
-        # users_count = db_table_rows_count(table_for_count)
-        #
-        # for user in range(1, users_count + 1):
-        #
-        #     # [x] 2. Проверяем статус on_hold, если он True - идём к следующему пользователю.
-        #     if check_on_hold(user):
-        #         continue
+        table_for_count = 'vda_users'
+        users_count = db_table_rows_count(table_for_count)
 
-            # [ ] 3. Получаем его last_chapter_sent параметр.
-            # TODO Можно реализовать через ту же функцию, что и получение on-hold-статуса
+        for user in range(1, users_count + 1):
 
-        # [ ] 4. Увеличиваем его на 1.
-        # [x] 5. Вытаскиваем соответствующий кусочек текста из таблицы с текстами, обращаясь по id.
+            # [x] 2. Проверяем статус on_hold, если он True - идём к следующему пользователю.
+            if get_from_db('on_hold', table_for_count, user):
+                continue
+
+            # [x] 3. Получаем его last_chapter_sent параметр.
+            print('user =', user)  # TEMPORARY STRING
+            last_chapter_sent = get_from_db('last_chapter_sent', table_for_count, user)
+            print('last chapter sent =', last_chapter_sent)    # TEMPORARY STRING
+
+            # [x] 4. Увеличиваем его на 1.
+            new_chapter_id = last_chapter_sent + 1
+            print('new chapter id = ', new_chapter_id)    # TEMPORARY STRING
+
+            # [x] 5. Вытаскиваем соответствующий кусочек текста из таблицы с текстами, обращаясь по id.
+            new_chapter = get_from_db('chapter', 'vda_messages', new_chapter_id)
+            print(new_chapter)  # TEMPORARY STRING
+
         # [ ] 6. Отправляем пользователю. Убеждаемся, что сообщение доставлено. (КАК?)
         # [ ] 7. Если сообщение не доставлено - возвращаемся к п. 5.
         # [x] 8. Если сообщение доставлено - обновляем last_chapter_sent для данного пользователя.
@@ -118,7 +132,7 @@ try:
         cell_name = 'last_chapter_sent'
         value = '6'
         row_id = '2'
-        db_update(table_name, cell_name, value, row_id)
+        # db_update(table_name, cell_name, value, row_id)
 
 
 except Error as e:
